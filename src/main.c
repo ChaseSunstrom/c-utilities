47dde246a941a4939aed3d23ebb7824a7fe1020d
ChaseSunstrom <casunstrom@gmail.com>
2024-07-25 00:09:31 -0500

    Fixed git ignore, added reminder to main

--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@ i32 main() {
   _Vector_T_push_back(p_vector, "Hello, World!");
   _Vector_T_push_back(p_vector, "Hi");
 
+  // Fix the result freeing,
+  // right now it does not take in stuff that would need to be freed in case of
+  // failure
   void *p_elem = Result_or_else(_Vector_T_at(p_vector, 1), _print_error);
 
   printf("Element at index 1: %s\n", p_elem);
