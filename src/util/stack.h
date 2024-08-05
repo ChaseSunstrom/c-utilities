245510283a72fc09c544bb1ce4108eee4a027eb7
Chase Sunstrom <122314695+ChaseSunstrom@users.noreply.github.com>
2024-08-05 12:50:00 -0500

    Update stack.h

--- src/util/stack.h
+++ src/util/stack.h
@@ -12,7 +12,7 @@ typedef struct {
 #define Stack_new(...) CONCAT(Stack_new_, NARGS(__VA_ARGS__))(__VA_ARGS__)
 
 Stack *Stack_new_1(Allocator *p_allocator);
-Stack *Stack_new_2(void (*destructor)(void *), Allocator *p_allocator);
+Stack *Stack_new_2(Dealloc_Fn destructor, Allocator *p_allocator);
 void Stack_free(Stack *p_stack);
 void Stack_free_(Stack **p_stack);
 void Stack_push(Stack *p_stack, void *p_data);
